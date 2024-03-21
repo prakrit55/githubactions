163a1f3b00d10797ff368a47c7a7c1899541ecbf
Griffin <prakritimandal611@gmail.com>
2024-03-21 07:09:36 +0530

    added
    Signed-off-by: Griffin <prakritimandal611@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {handleIssueComment} from './issueComment/handleIssueComment'
-import {handlePullReq} from './pullReq/handlePullReq'
+import {assign} from './issueComment/assign'
+// import {handlePullReq} from './pullReq/handlePullReq'
 
 
 async function run(): Promise<void> {
     try {
       switch (github.context.eventName) {
         case 'issue_comment':
-          handleIssueComment()
+          assign()
           break
   
-        case 'pull_request':
-          handlePullReq()
-          break
+        // case 'pull_request':
+        //   handlePullReq()
+        //   break
   
         default:
           core.error(`${github.context.eventName} not yet supported`)
           break
       }
     } catch (error) {
-      core.setFailed(error.message)
+      core.setFailed(String(error))
     }
   }
   
