75e53bfba87013ec4f39ee98fb92c62ab44777ab
Griffin <prakritimandal611@gmail.com>
2024-03-23 16:18:34 +0530

    add
    Signed-off-by: Griffin <prakritimandal611@gmail.com>

--- src/labels/onProgress.ts
+++ src/labels/onProgress.ts
@@ -11,6 +11,7 @@ import {getCurrentLabels, getIssueNummber, labelPresent, labelIssue} from '../ut
  */
 export const onPrOnReview = async (context: Context = github.context): Promise<void> => {
   const token = core.getInput('github-token', {required: true})
+  console.log(token)
   const octokit = new github.GitHub(token)
 
   const prNumber: number | undefined = context.payload.pull_request?.number
